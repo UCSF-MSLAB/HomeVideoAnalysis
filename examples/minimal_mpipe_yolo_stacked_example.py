0ea54b9b84c950d93511214789e5dd46d45e314d
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2024-05-22 21:45:48 -0700

    added an mpipe results handler. building out the 'minimal' stacked example.

--- examples/minimal_mpipe_yolo_stacked_example.py
+++ examples/minimal_mpipe_yolo_stacked_example.py
@@ -5,63 +5,12 @@ import mediapipe as mp
 from ultralytics import YOLO
 import pandas as pd
 
+from src.yolo_result_handler import YoloResultHandler as yrh
+from src.mpipe_result_handler import MPipeResultHandler as mprh
+
+MARGIN = 10
+
 MP4_PATH = os.getcwd() + "/tests/fixtures/Athletic Male Standard Walk Animation Reference Body Mechanics.mp4"
-
-MPIPE_LABELS = [
-    "nose",
-    "left_eye_inner",
-    "left_eye",
-    "left_eye_outer",
-    "right_eye_inner",
-    "right_eye",
-    "right_eye_outer",
-    "left_ear",
-    "right_ear",
-    "mouth_left",
-    "mouth_right",
-    "left_shoulder",
-    "right_shoulder",
-    "left_elbow",
-    "right_elbow",
-    "left_wrist",
-    "right_wrist",
-    "left_pinky",
-    "right_pinky",
-    "left_index",
-    "right_index",
-    "left_thumb",
-    "right_thumb",
-    "left_hip",
-    "right_hip",
-    "left_knee",
-    "right_knee",
-    "left_ankle",
-    "right_ankle",
-    "left_heel",
-    "right_heel",
-    "left_foot_index",
-    "right_foot_index"
-]
-
-YOLO_LABELS = [
-    "nose",
-    "left_eye",
-    "right_eye",
-    "left_ear",
-    "right_ear",
-    "left_shoulder",
-    "right_shoulder",
-    "left_elbow",
-    "right_elbow",
-    "left_wrist",
-    "right_wrist",
-    "left_hip",
-    "right_hip",
-    "left_knee",
-    "right_knee",
-    "left_ankle",
-    "right_ankle"
-]
 
 yolo_pose = YOLO(os.getcwd() + '/models/yolov8m-pose.pt')
 mp_pose = mp.solutions.pose.Pose(min_detection_confidence=0.5,
@@ -74,6 +23,13 @@ image.flags.writeable = False
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 yolo_results = yolo_pose(image)
+yolo_handle = yrh(0, yolo_results)
 
-yolo_results[0].keypoints
-mpipe_results = mp_pose.process(image)
+# get bounds for the mediapipe image
+mpipe_dfs = []
+for (xmin, ymin, xmax, ymax) in yolo_handle.boxes:
+    img_crop = image[int(ymin)+MARGIN:int(ymax)+MARGIN,int(xmin)+MARGIN:int(xmax)+MARGIN:]
+    mpipe_results = mp_pose.process(img_crop)
+    mpipe_handle = mprh(0, mpipe_results)
+    mpipe_dfs.append(mpipe_handle.df)
+
