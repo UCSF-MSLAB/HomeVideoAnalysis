aad421a0f71aefc5605e0ae5f05d8275459abb0c
Megan <megan.mccune@ucsf.edu>
2024-09-04 10:13:26 -0700

    Update to use vid_in_path for all output

--- megan_sandbox/sandbox_main_src_funs/landmark_visibility.py
+++ megan_sandbox/sandbox_main_src_funs/landmark_visibility.py
@@ -15,44 +15,48 @@ import os
 
 # import mediapipe df from csv 
 # yolo doesn't have visibility confidence score 
-#mp_all_filepath = r'..\temp\test_sandbox_pipeline_outputs\002_frames_to_time\DS_HC_gait_vertical_left_mediapipe_all.csv'
+#mp_all_filepath = r'..\..\temp\test_sandbox_pipeline_outputs\002_frames_to_time\DS_HC_gait_vertical_left_mediapipe_all.csv'
 #mp_all_df = pd.read_csv(mp_all_filepath, index_col = 0)
+
+# path to video 
+#vid_in_path = r'..\..\tests\fixtures\all_videos\DS_HC_practice videos\DS_HC_gait_vertical_left.mov'
 
 # output folder
 #output_parent_folder = r'..\temp\test_sandbox_pipeline_outputs'
 
 
-# In[4]:
+# In[3]:
 
 
 # plot visibility mediapipe 
-def vis_all_labels_boxplot(mp_all_df, mp_all_filepath): 
+def vis_all_labels_boxplot(mp_all_df, vid_in_path): 
     
     # save basename for plot title 
-    mp_basename = os.path.basename(mp_all_filepath)
+    vid_in_path_basename = os.path.basename(vid_in_path)
 
     # plot 
     plt.clf()
-    fig1, ax = plt.subplots()
-    ax = sns.boxplot(data=mp_all_df, x = 'label', y = 'vis')
+    fig1, ax1 = plt.subplots()
+    ax1 = sns.boxplot(data=mp_all_df, x = 'label', y = 'vis')
     plt.xticks(rotation=90)
-    plt.title('All Labels Visibility: ' + mp_basename)
+    plt.title('All Labels Visibility: ' + vid_in_path_basename)
 
     return(fig1)
 
 
-# In[5]:
+# In[4]:
 
 
 # save boxplot  
-def save_visibility_boxplot(boxplot, mp_all_filepath, output_parent_folder): 
+def save_visibility_boxplot(boxplot, vid_in_path, output_parent_folder): 
     output_folder = os.path.join(output_parent_folder, '003_landmark_visibility')
     
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     # create file name 
-    output_file_1 = os.path.normpath(os.path.join(output_folder, 'mp_visibility_boxplot.png'))
+    input_file_no_ext_1 = os.path.splitext(os.path.basename(vid_in_path))[0]
+    output_file_1 = os.path.normpath(os.path.join(output_folder, input_file_no_ext_1 +'_mp_visibility_boxplot.png'))
 
     # save figure 
     boxplot.savefig(output_file_1, bbox_inches = 'tight')
@@ -61,13 +65,13 @@ def save_visibility_boxplot(boxplot, mp_all_filepath, output_parent_folder):
     
 
 
-# In[6]:
+# In[5]:
 
 
 # plot visibility - line plot 
-def vis_lineplot(mp_all_df, mp_all_filepath): 
+def vis_lineplot(mp_all_df, vid_in_path): 
     # save basename for plot title 
-    mp_basename = os.path.basename(mp_all_filepath)
+    vid_in_path_basename = os.path.basename(vid_in_path)
     
     # change label to string for future filtering 
     mp_all_df['label'] = mp_all_df['label'].astype(str)
@@ -86,23 +90,24 @@ def vis_lineplot(mp_all_df, mp_all_filepath):
     fig2, ax = plt.subplots()
     ax = sns.lineplot(data=mp_all_df, x='frame', y='vis', hue='label', markers=True, dashes=False, estimator = None)
     plt.legend(loc = 'right')
-    plt.title('All Labels Visibility: ' + mp_basename)
+    plt.title('All Labels Visibility: ' + vid_in_path_basename)
 
     return(fig2)
 
 
-# In[7]:
+# In[6]:
 
 
 # save lineplot 
-def save_visibility_lineplot(lineplot, mp_all_filepath, output_parent_folder): 
+def save_visibility_lineplot(lineplot, vid_in_path, output_parent_folder): 
     output_folder = os.path.join(output_parent_folder, '003_landmark_visibility')
     
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     # create file name 
-    output_file_2 = os.path.normpath(os.path.join(output_folder, 'mp_visibility_lineplot.png'))
+    input_file_no_ext_2 = os.path.splitext(os.path.basename(vid_in_path))[0]
+    output_file_2 = os.path.normpath(os.path.join(output_folder, input_file_no_ext_2 +'_mp_visibility_lineplot.png'))
 
     # save figure 
     lineplot.savefig(output_file_2, bbox_inches = 'tight')
@@ -110,7 +115,7 @@ def save_visibility_lineplot(lineplot, mp_all_filepath, output_parent_folder):
     plt.close()
 
 
-# In[8]:
+# In[7]:
 
 
 # save vis score stats as df 
@@ -141,49 +146,57 @@ def save_vis_stats_by_label(mp_all_df):
     
 
 
-# In[9]:
+# In[8]:
 
 
 # save vis score stats as 
-def save_vis_scores_csv(vis_stats_df, mp_all_filepath, output_parent_folder):
+def save_vis_scores_csv(vis_stats_df, vid_in_path, output_parent_folder):
     output_folder = os.path.join(output_parent_folder, '003_landmark_visibility')
     
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
      # create file name 
-    output_file_3 = os.path.normpath(os.path.join(output_folder, 'mp_visibility_stats.csv'))
+    input_file_no_ext_3 = os.path.splitext(os.path.basename(vid_in_path))[0]
+    output_file_3 = os.path.normpath(os.path.join(output_folder, input_file_no_ext_3 + '_mp_visibility_stats.csv'))
 
     # save csv 
     vis_stats_df.to_csv(output_file_3)
 
 
-# In[10]:
+# In[9]:
 
 
 ## run functions 
 
 
-# In[11]:
+# In[10]:
 
 
 # boxplot 
-#boxplot = vis_all_labels_boxplot(mp_all_df, mp_all_filepath)
-#save_visibility_boxplot(boxplot, mp_all_filepath, output_parent_folder)
+#boxplot = vis_all_labels_boxplot(mp_all_df, vid_in_path)
+#save_visibility_boxplot(boxplot, vid_in_path, output_parent_folder)
+
+
+# In[11]:
+
+
+# lineplot 
+#lineplot = vis_lineplot(mp_all_df, vid_in_path)
+#save_visibility_lineplot(lineplot, vid_in_path, output_parent_folder)
 
 
 # In[12]:
 
 
-# lineplot 
-#lineplot = vis_lineplot(mp_all_df, mp_all_filepath)
-#save_visibility_lineplot(lineplot, mp_all_filepath, output_parent_folder)
+# calculate and save vis score per label 
+#vis_stats_df = save_vis_stats_by_label(mp_all_df)
+#save_vis_scores_csv(vis_stats_df, vid_in_path, output_parent_folder)
 
 
 # In[13]:
 
 
-# calculate and save vis score per label 
-#vis_stats_df = save_vis_stats_by_label(mp_all_df)
-#save_vis_scores_csv(vis_stats_df, mp_all_filepath, output_parent_folder)
+## convert to .py file so functions can be used in other scripts 
+get_ipython().system('jupyter nbconvert --to script landmark_visibility.ipynb')
 
--- megan_sandbox/sandbox_main_src_funs/merge_mp_yolo_dfs.py
+++ megan_sandbox/sandbox_main_src_funs/merge_mp_yolo_dfs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 import pandas as pd 
@@ -9,17 +9,20 @@ import numpy as np
 import os 
 
 
-# In[2]:
+# In[ ]:
 
 
 # input 
 # load mp, mp_world, and yolo .csv files for one video (from main branch, output of home video analysis run.py 8/29/2024)
-#mp_pose_filepath = r'..\temp\main_branch_outputs\000_run\DS_HC_gait_vertical_left_mediapipe.csv'
-#mp_world_filepath = r'..\temp\main_branch_outputs\000_run\DS_HC_gait_vertical_left_mediapipe_world.csv'
-#yolo_filepath = r'..\temp\main_branch_outputs\000_run\DS_HC_gait_vertical_left_yolo.csv'
+#mp_pose_filepath = r'..\..\temp\main_branch_outputs\000_run\DS_HC_gait_vertical_left_mediapipe.csv'
+#mp_world_filepath = r'..\..\temp\main_branch_outputs\000_run\DS_HC_gait_vertical_left_mediapipe_world.csv'
+#yolo_filepath = r'..\..\temp\main_branch_outputs\000_run\DS_HC_gait_vertical_left_yolo.csv'
+
+# path to video 
+#vid_in_path = r'..\..\tests\fixtures\all_videos\DS_HC_practice videos\DS_HC_gait_vertical_left.mov'
 
 # output folder 
-#output_parent_folder = r'..\temp\test_sandbox_pipeline_outputs'
+#output_parent_folder = r'..\..\temp\test_sandbox_pipeline_outputs'
 
 # read csv
 #mp_pose_df = pd.read_csv(mp_pose_filepath)
@@ -28,10 +31,10 @@ import os
     
 
 
-# In[3]:
+# In[ ]:
 
 
-# input = mp_pose, mp_world, and yolo filepaths for one video 
+# input = mp_pose, mp_world, and yolo df for one video  
 # output = one mediapipe df and one yolo df, both with positive and negative Y values  
 def merge_mp_pose_world(mp_pose_df, mp_world_df, yolo_df):
     
@@ -63,19 +66,19 @@ def merge_mp_pose_world(mp_pose_df, mp_world_df, yolo_df):
     return([mp_all_df, yolo_df])
 
 
-# In[4]:
+# In[ ]:
 
 
 ## add video direction and turn direction to data frame 
-def add_orientation_and_turn_direction(mp_pose_filepath, mp_all_df, yolo_df):
+def add_orientation_and_turn_direction(vid_in_path, mp_all_df, yolo_df):
 
-    mp_basename = os.path.basename(mp_pose_filepath)
+    vid_in_path_basename = os.path.basename(vid_in_path)
     
     # add camera orientation from file name 
-    if 'horizon' in mp_basename:
+    if 'horizon' in vid_in_path_basename:
         mp_all_df['camera_orientation'] = 'horizontal'
         yolo_df['camera_orientation'] = 'horizontal'
-    elif 'vert' in mp_basename: 
+    elif 'vert' in vid_in_path_basename: 
         mp_all_df['camera_orientation'] = 'vertical'
         yolo_df['camera_orientation'] = 'vertical'
     else: 
@@ -83,10 +86,10 @@ def add_orientation_and_turn_direction(mp_pose_filepath, mp_all_df, yolo_df):
         yolo_df['camera_orientation'] = 'UNK'
 
     # add turn direction from file name 
-    if 'left' in mp_basename:
+    if 'left' in vid_in_path_basename:
         mp_all_df['turn_direction'] = 'left'
         yolo_df['turn_direction'] = 'left'
-    elif 'right' in mp_basename: 
+    elif 'right' in vid_in_path_basename: 
         mp_all_df['turn_direction'] = 'right'
         yolo_df['turn_direction'] = 'right'
     else: 
@@ -96,36 +99,36 @@ def add_orientation_and_turn_direction(mp_pose_filepath, mp_all_df, yolo_df):
     return([mp_all_df, yolo_df])
 
 
-# In[5]:
+# In[ ]:
 
 
 # save outputs 
-def save_merge_mp_yolo_df(mp_pose_filepath, mp_all_df, yolo_filepath, yolo_df, output_parent_folder):
+def save_merge_mp_yolo_df(mp_all_df, yolo_df, vid_in_path, output_parent_folder):
 
     output_folder = os.path.join(output_parent_folder, '001_merge_mp_yolo_dfs')
     
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
+
+    vid_in_path_no_ext = os.path.splitext(os.path.basename(vid_in_path))[0]
     
     # save merged mp df as csv
-    input_file_no_ext_1 = os.path.splitext(os.path.basename(mp_pose_filepath))[0]
-    output_file_name_1 = input_file_no_ext_1 + '_all.csv'
+    output_file_name_1 = vid_in_path_no_ext + '_mediapipe_all.csv'
     output_file_1 = os.path.normpath(os.path.join(output_folder, output_file_name_1))
     mp_all_df.to_csv(output_file_1)
     
     # save yolo df as csv  
-    input_file_no_ext_2 = os.path.splitext(os.path.basename(yolo_filepath))[0]
-    output_file_name_2 = input_file_no_ext_2 + '.csv'
+    output_file_name_2 = vid_in_path_no_ext + '_yolo.csv'
     output_file_2 = os.path.normpath(os.path.join(output_folder, output_file_name_2))
     yolo_df.to_csv(output_file_2)
 
 
-# In[6]:
+# In[ ]:
 
 
 #[mp_all_df, yolo_df] = merge_mp_pose_world(mp_pose_df, mp_world_df, yolo_df)
-#[mp_all_df, yolo_df] = add_orientation_and_turn_direction(mp_pose_filepath, mp_all_df, yolo_df)
-#save_merge_mp_yolo_df(mp_pose_filepath, mp_all_df, yolo_filepath, yolo_df, output_parent_folder)
+#[mp_all_df, yolo_df] = add_orientation_and_turn_direction(vid_in_path, mp_all_df, yolo_df)
+#save_merge_mp_yolo_df(mp_all_df, yolo_df, vid_in_path, output_parent_folder)
 
 
 # In[ ]:
