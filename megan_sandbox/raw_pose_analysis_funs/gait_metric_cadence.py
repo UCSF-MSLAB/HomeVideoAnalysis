86d02549981255ca5c78049f0d0528dddfd043e7
Megan <megan.mccune@ucsf.edu>
2025-01-15 11:30:09 -0800

    Testing cadence change and saving video time
    - save total video time
    - cadence: testing steps - 1, to steps +1

--- megan_sandbox/raw_pose_analysis_funs/gait_metric_cadence.py
+++ megan_sandbox/raw_pose_analysis_funs/gait_metric_cadence.py
@@ -16,8 +16,10 @@ import os
 # PKMAS - The number of footfalls minus one divided by the ambulation time, converted to minutes (steps/min). 
 # Cadence is shown on the “Mean” row of the Gait Table
 
+# peaks and valleys + 1 - stride time algorithm often misses first or last stride 
+
 def calculate_cadence(stride_times_peaks, stride_times_valleys, start_sec, end_sec, vid_in_path, output_parent_folder): 
-    total_steps = (len(stride_times_peaks) + len(stride_times_valleys)) - 1 # peaks + valleys
+    total_steps = (len(stride_times_peaks) + len(stride_times_valleys)) + 1 # peaks + valleys + 1 
     video_length_sec = end_sec - start_sec # video length 
     cadence = (total_steps/video_length_sec) * 60
     cadence_df = pd.DataFrame(index = range(1),
