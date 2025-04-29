7a9256afa889bcac518016f0bebb4cef86997be1
Megan <megan.mccune@ucsf.edu>
2025-04-29 13:29:00 -0700

    Updated run_MM file for BW videos

--- run_MM.py
+++ run_MM.py
@@ -21,7 +21,7 @@ ALLOWED_VID_FORMATS = ["asf", "avi", "gif", "m4v",
                        "mkv", "mov", "mp4", "mpeg",
                        "mpg", "ts", "wmv", "webm"]
 
-MODELS = ["yolo", "mediapipe", "mediapipe_world"]
+MODELS = ["yolo", "mediapipe", "mediapipe_world", "marigold"]
 
 
 def get_frames_per_second(vid_in_path): 
@@ -32,7 +32,7 @@ def get_frames_per_second(vid_in_path):
     
     return([fps,fps_df])
 
-def process_dir(dir_in_path, dir_out_path):
+def process_dir(dir_in_path, dir_out_path, run_depth):
 
     for (dir_path, dir_names, file_names) in os.walk(dir_in_path):
         for file_name in file_names:
@@ -41,16 +41,16 @@ def process_dir(dir_in_path, dir_out_path):
             # 10/8 - no TUG, DTW, QSLOS tasks from in-person video, just PWS and FWS from camera #1
             if (ext in ALLOWED_VID_FORMATS) & ('gait_vertical' in name) & ('DTW' not in name) & ('QSLOS' not in name) & ('TUG' not in name) & ('FW_2' not in name) & ('PWS_2' not in name):
                 vid_in_path = os.path.join(dir_path, file_name) # full path to video with file name 
-                print('vid_in_path:' + vid_in_path)
+                print(f"vid_in_path: {vid_in_path}")
                 vid_relpath= os.path.relpath(vid_in_path, dir_in_path) # relative path, with file name and extension
-                print('vid_relpath:' + vid_relpath)
+               # print('vid_relpath:' + vid_relpath)
                 vid_subfolders = os.path.dirname(vid_relpath) # relative path with folders only, no file name
-                print('vid_subfolders:' + vid_subfolders)
+               # print('vid_subfolders:' + vid_subfolders)
                 data_out_prefix = os.path.normpath(os.path.join(dir_out_path, vid_subfolders)) # create output folder that mirrors subfolder structure from dir_in_path
-                print('data_out_prefix:' + data_out_prefix)
+                print(f"data_out_prefix {data_out_prefix}")
                 if not os.path.exists(data_out_prefix):
                     os.makedirs(data_out_prefix) # make directory if it doesn't exist already
-                    print('making directory: ' + data_out_prefix)
+                    print(f"Making directory: {data_out_prefix}")
                 print(f"Processing: {file_name}")
 
                 # make folder for raw pose estimation data 
@@ -60,7 +60,6 @@ def process_dir(dir_in_path, dir_out_path):
                 # if raw pose directory does not exist yet, make folder 
                 if not os.path.exists(raw_data_out_folder):
                     os.makedirs(raw_data_out_folder) # make directory if it doesn't exist already
-                    print('making directory: ' + raw_data_out_folder)
 
                 # get frames per second from video 
                 [fps,fps_df] = get_frames_per_second(vid_in_path)
@@ -68,7 +67,7 @@ def process_dir(dir_in_path, dir_out_path):
                 fps_df.to_csv(os.path.normpath(os.path.join(raw_data_out_folder, name + '_fps.csv')))
 
                 # run pose estimation on video and save raw data as .csv file 
-                model_results = extract_pose_data(vid_in_path)
+                model_results = extract_pose_data(vid_in_path, run_depth)
                 for i, raw_data in enumerate(model_results):
                     try:
                         pose_data = transform_pose_data(raw_data)
@@ -97,15 +96,20 @@ def main():
     args = sys.argv[1:]
     if len(args) < 2 or args[0] == "--help":
         # python3 -W ignore hva.py ./tmp/fixtures ./tmp/csv_output
-        print("usage: python3 run.py <DIR_IN_PATH> <DIR_OUT_PATH>")
+        print("usage: python3 run.py <DEPTH: T/F> <DIR_IN_PATH> <DIR_OUT_PATH>")
         exit()
 
-    dir_in_path = args[0]
-    dir_out_path = args[1]
+    if not args[0] in ["T", "F"]:
+        print("Please specific if Depth Estimates should be made: T or F")
+        sys.exit()
+
+    run_depth = (args[0] == "T")
+    dir_in_path = args[1]
+    dir_out_path = args[2]
 
     print(f"Processing files in {dir_in_path}...")
     # process_folder(in_folder, out_folder)
-    process_dir(dir_in_path, dir_out_path)
+    process_dir(dir_in_path, dir_out_path, run_depth)
     logger.info("Finished")
 
 
